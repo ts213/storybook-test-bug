710062e84898a286b90b669f8bad0f04fd7b58ed
ts213 <107356620+ts213@users.noreply.github.com>
2023-12-19 13:36:57 +0300

    set up msw mock

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,27 @@
+import { initialize, mswLoader } from 'msw-storybook-addon'
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
+        date: /Date$/
+      }
+    }
   },
-};
+  loaders: [mswLoader]
+}
 
-export default preview;
+export default preview
+
+initialize({
+  onUnhandledRequest: (request, callback) => {
+    if (!["script"].includes(request.destination)) {
+      console.log(1111111, request.destination, request.url)
+      console.error('-------------- unhandled fetch request ------------');
+      callback.error();
+    }
+  }
+})
--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -2,7 +2,6 @@ import { within, userEvent } from '@storybook/testing-library';
 import MyPage from './Page.vue';
 
 export default {
-  title: 'Example/Page',
   component: MyPage,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/vue/configure/story-layout
@@ -10,10 +9,8 @@ export default {
   },
 };
 
-export const LoggedOut = {};
-
 // More on interaction testing: https://storybook.js.org/docs/vue/writing-tests/interaction-testing
-export const LoggedIn = {
+export const Test = {
   render: () => ({
     components: {
       MyPage,
@@ -21,10 +18,5 @@ export const LoggedIn = {
     template: '<my-page />',
   }),
   play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
-      name: /Log in/i,
-    });
-    await userEvent.click(loginButton);
   },
 };
